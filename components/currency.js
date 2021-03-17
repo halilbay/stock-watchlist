b48521e07c46daa14f9655c86f03cd3081138fee
Halil Bay <halilbay91@gmail.com>
2021-03-17 20:28:59 +0300

    currency items implemented
    currency svgs are added,
    yarn icon command converts to js file,
    select options are added

--- components/currency.js
+++ components/currency.js
@@ -1,14 +1,31 @@
 import React from 'react'
 import { Select } from 'antd'
-
+import { Try, Usd, Eur, Gbp } from './icons'
 const { Option } = Select
 const Currency = () => {
+  const handleChange = (value) => {
+    console.log(value)
+  }
+
   return (
-    <Select>
-      <Option></Option>
-      <Option></Option>
-      <Option></Option>
-      <Option></Option>
+    <Select defaultValue={false} style={{ width: 240 }} onChange={handleChange}>
+      <Option value={false}>Select Currency</Option>
+      <Option value="TRY">
+        <Try />
+        Turkish Lira
+      </Option>
+      <Option value="USD">
+        <Usd />
+        US Dollar
+      </Option>
+      <Option value="EUR">
+        <Eur />
+        Euro
+      </Option>
+      <Option value="GBP">
+        <Gbp />
+        Pound
+      </Option>
     </Select>
   )
 }
---
+++ components/icons/index.js
@@ -0,0 +1,4 @@
+export { default as Eur } from './Eur'
+export { default as Gbp } from './Gbp'
+export { default as Try } from './Try'
+export { default as Usd } from './Usd'
